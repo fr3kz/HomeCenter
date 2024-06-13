7da44a83d45657c09d62d2a0ed39644bcfc0c9b8
Artur Kunicki <tektmap1@gmail.com>
2024-06-13 22:04:55 +0200

    .

---
+++ Data/Migrations/20240525200139_AddRelations.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HomeCenter.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRelations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SavingTargets_Budgets_BudgetModelId",
+                table: "SavingTargets");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "BudgetModelId",
+                table: "SavingTargets",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "INTEGER",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SavingTargets_Budgets_BudgetModelId",
+                table: "SavingTargets",
+                column: "BudgetModelId",
+                principalTable: "Budgets",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SavingTargets_Budgets_BudgetModelId",
+                table: "SavingTargets");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "BudgetModelId",
+                table: "SavingTargets",
+                type: "INTEGER",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SavingTargets_Budgets_BudgetModelId",
+                table: "SavingTargets",
+                column: "BudgetModelId",
+                principalTable: "Budgets",
+                principalColumn: "Id");
+        }
+    }
+}
